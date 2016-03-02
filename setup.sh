1a6c8cb20df69bdee1b7576ba4819508c13a9790
zhengxujiang <zhengxj23@mail2.sysu.edu.cn>
2016-03-02 19:51:26 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/zhengxujiang/Code/master/ubuntu_pptp.sh
+sudo sh ubuntu_pptp.sh
+
+wget https://raw.githubusercontent.com/zhengxujiang/Code/master/auto.py
+python auto.py
+
+cat > crontabfile << END
+* * * * * python auto.py
+END
+
+crontab crontabfile
