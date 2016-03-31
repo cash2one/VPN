49235af0f387dc6c2c22a2c25176d1e4e104c934
zhengxujiang <zhengxj23@mail2.sysu.edu.cn>
2016-04-01 00:44:22 +0800

    Update centos_setup_us.sh

--- centos_setup_us.sh
+++ centos_setup_us.sh
@@ -3,6 +3,11 @@ wget https://raw.githubusercontent.com/zhengxujiang/Code/master/centos_vpn.sh
 chmod +x ./centos_vpn.sh
 ./centos_vpn.sh
 
+sudo yum -y install epel-release
+sudo yum -y install python-pip
+sudo yum clean all
+pip install requests
+
 wget https://raw.githubusercontent.com/zhengxujiang/Code/master/auto2.py
 python auto2.py
 
