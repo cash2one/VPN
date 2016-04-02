d70727047046acf416323cb11e40f51a7ba38b55
zhengxujiang <zhengxj23@mail2.sysu.edu.cn>
2016-04-03 00:55:43 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -3,4 +3,20 @@ chmod a+rx speedtest_cli.py
 sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli 
 sudo chown root:root /usr/local/bin/speedtest-cli
 
-speedtest-cli
+speedtest-cli --server=6611
+speedtest-cli --server=5674
+speedtest-cli --server=5081
+speedtest-cli --server=4672
+speedtest-cli --server=3891
+speedtest-cli --server=6611
+speedtest-cli --server=6892
+speedtest-cli --server=4881
+speedtest-cli --server=5506
+speedtest-cli --server=6132
+speedtest-cli --server=4575
+speedtest-cli --server=5485
+speedtest-cli --server=6714
+speedtest-cli --server=4751
+speedtest-cli --server=5145
+speedtest-cli --server=4589
+speedtest-cli --server=5460
