c8a6af2825793848e3289fde11a8b15d298a4660
zhengxujiang <zhengxj23@mail2.sysu.edu.cn>
2016-04-03 00:37:06 +0800

    Create speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,6 @@
+wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py 
+chmod a+rx speedtest_cli.py
+sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli 
+sudo chown root:root /usr/local/bin/speedtest-cli
+
+speedtest-cli
