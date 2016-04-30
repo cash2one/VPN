1a5b0f82601aa292c4dfc17fa61f20d252943ed8
zhengxujiang <zhengxj23@mail2.sysu.edu.cn>
2016-04-30 11:49:37 +0800

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -5,7 +5,7 @@ wget https://raw.githubusercontent.com/zhengxujiang/Code/master/auto2.py
 python auto2.py
 
 cat > crontabfile << END
-00 13 * * * python auto2.py
+00 0,2,4,8,10,12,14,16,18,20,22 * * * python auto2.py
 END
 
 crontab crontabfile
