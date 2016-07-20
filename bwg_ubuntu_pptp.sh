6931c28dee129a046ccaab0a5367c94d5ee74175
zhengxujiang <11456354@qq.com>
2016-07-20 17:37:08 +0800

    Update bwg_ubuntu_pptp.sh

--- bwg_ubuntu_pptp.sh
+++ bwg_ubuntu_pptp.sh
@@ -73,7 +73,7 @@ iptables -I INPUT -p tcp --dport 1723 -j ACCEPT
 #gre tunnel protocol
 iptables -I INPUT  --protocol 47 -j ACCEPT
 
-iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -d 0.0.0.0/0 -ovenet0 -j MASQUERADE
 
 #supposedly makes the vpn work better
 iptables -I FORWARD -s 192.168.2.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
