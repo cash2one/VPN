086395aa75503b8bab769e6eb1615be067e03c1a
zhengxujiang <11456354@qq.com>
2016-07-22 13:00:52 +0800

    Create pptp.sh

---
+++ pptp.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install pptpd
+
+sed -i '$a localip 10.10.0.1' /etc/pptpd.conf
+sed -i '$a remoteip 10.10.0.2-255' /etc/pptpd.conf
+
+sed -i '$a ms-dns 8.8.8.8' /etc/ppp/pptpd-options
+
+sed -i '$a zxj * zxj1234 *' /etc/ppp/chap-secrets
+
+sed -i '$a net.ipv4.ip_forward=1' /etc/sysctl.conf
+sudo sysctl -p
